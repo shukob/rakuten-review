ecc1a8223760dc2801093f1a6d5e60b250527f24
Shunpei Kobayashi <skonb@me.com>
2015-07-17 18:52:13 +0900

    Initial commit

---
+++ lib/reakuten/review.rb
@@ -0,0 +1,7 @@
+require "reakuten/review/version"
+
+module Reakuten
+  module Review
+    # Your code goes here...
+  end
+end
---
+++ lib/reakuten/review/version.rb
@@ -0,0 +1,5 @@
+module Reakuten
+  module Review
+    VERSION = "0.1.0"
+  end
+end
